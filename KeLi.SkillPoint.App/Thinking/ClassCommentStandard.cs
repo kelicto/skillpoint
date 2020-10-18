36b890e3179e53ca36075812a12b4ed78551af8d
KeLi <kelicto@protonmail.com>
2020-10-18 14:54:50 +0800

    Adds 'ClassCommentStandard'.

---
+++ KeLi.SkillPoint.App/Thinking/ClassCommentStandard.cs
@@ -0,0 +1,58 @@
+﻿namespace KeLi.SkillPoint.App.Thinking
+{
+    /// <summary>
+    ///     Class comment standard.
+    /// </summary>
+    public class ClassCommentStandard
+    {
+        /// <summary>
+        ///     It's for Rule A.
+        /// </summary>
+        /// <RevisionHistory>
+        ///     <RevisionItem>
+        ///         <Author>Wang Wu</Author>
+        ///         <Date>2020.10.03</Date>
+        ///         <Reason>
+        ///             The id of rule is 'M150102CK'.
+        ///         </Reason>
+        ///     </RevisionItem>
+        /// </RevisionHistory>
+        public void RuleA()
+        {
+            // Some code...
+        }
+
+        /// <summary>
+        ///     It's for Rule B.
+        /// </summary>
+        /// <RevisionHistory>
+        ///     <RevisionItem>
+        ///         <Author>Wang Wu</Author>
+        ///         <Date>2020.10.05</Date>
+        ///         <Reason>
+        ///             The id of rule is 'M150102CK'.
+        ///         </Reason>
+        ///     </RevisionItem>
+        /// 
+        ///     <RevisionItem>
+        ///         <Author>Wang Wu</Author>
+        ///         <Date>2020.10.05</Date>
+        ///         <Reason>
+        ///             The rule changed，The id is 'M150106CK'.
+        ///         </Reason>
+        ///     </RevisionItem>
+        ///
+        ///     <RevisionItem>
+        ///         <Author>Wang Wu</Author>
+        ///         <Date>2020.10.05</Date>
+        ///         <Reason>
+        ///             The rule changed，The id is 'M150113CK'.
+        ///         </Reason>
+        ///     </RevisionItem>
+        /// </RevisionHistory>
+        public void RuleB()
+        {
+            // Some code...
+        }
+    }
+}
